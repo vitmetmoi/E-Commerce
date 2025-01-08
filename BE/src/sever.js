40e6a48d3930ffc724d04d3c6ac82f15ae0a81da
sansukai <lobaoduy2017@gmail.com>
2025-01-08 15:26:38 +0700

    create 10 files, update 2 files, delete 2 files and move 2 files

--- BE/src/sever.js
+++ BE/src/sever.js
@@ -1,8 +1,9 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
-import initWebRoutes from "./route/web";
-import initApiRoutes from './route/api'
-import configCors from './config/cors'
+import initWebRoutes from "./routes/web";
+import initApiRoutes from './routes/api';
+import configCors from './config/cors';
+import connectToDataBase from './config/connectDb';
 require('dotenv').config();
 const app = express();
 var bodyParser = require('body-parser');
@@ -14,8 +15,21 @@ configCors(app);
 //config view engine
 configViewEngine(app);
 
-const PORT = process.env.PORT || 8080;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+//cookie parse
+app.use(cookieParser())
+//init web routes
+initWebRoutes(app);
+
+//init api routes
+initApiRoutes(app);
+const PORT = process.env.PORT || 8080;
+connectToDataBase();
 app.listen(PORT, () => {
     console.log(">>> Backend is running on the port = " + PORT);
 })
