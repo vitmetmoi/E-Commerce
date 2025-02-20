b8558e24b9b22de26b9cda5fd680d6a91e5ada61
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-02-20 22:54:03 +0700

    update ShoppingCart.js and shoppingCartSilce.js

--- FE/src/container/product/ShoppingCart.js
+++ FE/src/container/product/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ShoppingCart.scss';
 import AdsHome from '../home/AdsHome';
 import NavigationHome from '../home/NavigationHome';
@@ -6,16 +6,18 @@ import Footer from '../home/Footer';
 import { useSelector, useDispatch } from 'react-redux';
 import { useGetClothesDataMutation } from '../../store/slice/API/systemAPI';
 import _ from 'lodash'
-import { setShoppingCart, setShoppingCartData } from '../../store/slice/Reducer/shoppingCartSilce';
+import { setShoppingCart } from '../../store/slice/Reducer/shoppingCartSilce';
 
 function ShoppingCart(props) {
     const dispatch = useDispatch();
     const [getClothesService, { data, isLoading }] = useGetClothesDataMutation();
     const shoppingCart = useSelector((state) => state.shoppingCart.shoppingCart);
-    const shoppingCartData = useSelector((state) => state.shoppingCart.shoppingCartData);
+    const [shoppingCartData, setShoppingCartData] = useState('');
     // console.log("shop", shoppingCartData)
     useEffect(() => {
+
         if (_.isEmpty(shoppingCartData) || shoppingCartData[0] === '') {
+            console.log("123")
             if (shoppingCart && !_.isEmpty(shoppingCart)) {
                 getClothes();
             }
@@ -24,7 +26,6 @@ function ShoppingCart(props) {
 
     const getClothes = async () => {
         let arrId = [];
-        console.log('shop cart', shoppingCart);
         shoppingCart.map(item1 => {
             let isExist = false;
             arrId.map(item2 => {
@@ -36,22 +37,21 @@ function ShoppingCart(props) {
                 arrId.push(item1.clothesId);
             }
         })
-        console.log('arr id', arrId)
 
         if (arrId) {
 
             let arrDataClothes = [];
+
             arrId.map(async (item) => {
                 console.log(item);
                 let res = await getClothesService({ type: 'ONE', id: item });
-                if (res && res.data && res.data.EC === 0) {
+                if (res && res.data && res.data.EC === 0 && res.data.DT) {
                     arrDataClothes.push(res.data.DT);
                 }
             })
-            if (arrDataClothes && isLoading === false) {
-                console.log('dt', arrDataClothes)
 
-                dispatch(setShoppingCartData(arrDataClothes));
+            if (arrDataClothes) {
+                setShoppingCartData(arrDataClothes);
             }
         }
 
