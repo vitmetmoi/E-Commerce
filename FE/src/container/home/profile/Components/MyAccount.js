99f152077c0c1bbf7b3eff8231fccb8a08d4e5e1
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-02-26 00:39:25 +0700

    update MyAccount.js, MyAccount.scss and UserProfile.js

--- FE/src/container/home/profile/Components/MyAccount.js
+++ FE/src/container/home/profile/Components/MyAccount.js
@@ -1,10 +1,68 @@
 import React from 'react';
-
+import './MyAccount.scss';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+import { IconButton } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux'
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import RateReviewIcon from '@mui/icons-material/RateReview';
 function MyAccount(props) {
+
+    const userData = useSelector((state) => state.user.userData);
+    const dispatch = useDispatch();
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+            width: '35px',
+            height: '35px',
+            borderRadius: '100px',
+            left: '50%',
+            bottom: '10px',
+            border: `2px solid ${theme.palette.background.paper}`,
+            padding: '0 4px',
+        },
+    }));
+
     return (
-        <div>
-            my account
-        </div>
+        <>
+            <div className='my-account-container'>
+
+
+                <div className='title-content'>
+                    <h5 className='title'>Personal Information</h5>
+                </div>
+
+                <div className='profile-content'>
+
+                    <div className='content-left'>
+
+                        <div className='change-avt'>
+                            <IconButton aria-label="cart">
+                                <StyledBadge
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'right',
+                                    }}
+                                    badgeContent={<ChangeCircleIcon />} color="primary">
+                                    <img className='avt' src={userData.avatar}></img>
+                                </StyledBadge>
+                            </IconButton>
+                        </div>
+
+                    </div>
+
+                    <div className='content-right'>
+                        <div className='change-btn'>
+
+                            <RateReviewIcon />
+                            <span>Change Profile Information</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 }
 
--- FE/src/container/home/profile/UserProfile.js
+++ FE/src/container/home/profile/UserProfile.js
@@ -16,7 +16,44 @@ import MyRatingAndReviews from './Components/MyRatingAndReviews';
 import ChangePassword from './Components/ChangePassword';
 import { clearUserData } from '../../../store/slice/Reducer/userSlice';
 import LogoutIcon from '@mui/icons-material/Logout';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+
+
 function UserProfile(props) {
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+            width: '12px',
+            height: '12px',
+            borderRadius: '10px',
+            backgroundColor: '#44b700',
+            color: '#44b700',
+            boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+            '&::after': {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+                animation: 'ripple 1.2s infinite ease-in-out',
+                border: '1px solid currentColor',
+                content: '""',
+            },
+        },
+        '@keyframes ripple': {
+            '0%': {
+                transform: 'scale(.8)',
+                opacity: 1,
+            },
+            '100%': {
+                transform: 'scale(2.4)',
+                opacity: 0,
+            },
+        },
+    }));
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -50,8 +87,16 @@ function UserProfile(props) {
 
                         <div className='user-avt'>
                             <div className='avt-container'>
-                                <img className='user-avt' src={userData.avatar}></img>
+
+                                <StyledBadge
+                                    overlap="circular"
+                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                                    variant="dot"
+                                >
+                                    <img className='user-avt' src={userData.avatar}></img>
+                                </StyledBadge>
                             </div>
+
                             <div className='name-container'>
                                 <span className='text-1'>Hello</span>
                                 <span className='text-2'>{userData.firstName} {userData.lastName}</span>
